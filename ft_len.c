f71f0ad0c5b2834b9f1716a6e4781517428913d6
lmoi <s4kaluu@gmail.com>
2020-10-13 23:03:05 +0200

    unsigned positif fonctionne

--- ft_len.c
+++ ft_len.c
@@ -6,7 +6,7 @@
 /*   By: lmoi <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/09 00:29:46 by lmoi              #+#    #+#             */
-/*   Updated: 2020/10/13 18:13:42 by lmoi             ###   ########.fr       */
+/*   Updated: 2020/10/13 23:00:53 by lmoi             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,7 @@ int	len_base(unsigned int nb, int b)
 
 int	ft_len(char c, t_printf *pr)
 {
-	if (corresponding(c, "di") == 1)
+	if (corresponding(c, "diu") == 1)
 		return (len_nbr(va_arg(pr->pa, int), pr));
 	if (corresponding(c, "xX"))
 		return (len_base(va_arg(pr->pa, unsigned int), 16));
