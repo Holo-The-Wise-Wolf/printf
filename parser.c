9d078912c8c5dc745342d3eb688a382b99769375
lmoi <s4kaluu@gmail.com>
2020-10-19 19:20:17 +0200

    adds parse_precision and changes other parse functions accordingly to ft_printf.h changes

--- parser.c
+++ parser.c
@@ -4,6 +4,7 @@ void	t_arg_init(t_arg *arg)
 {
 	arg->flags		= 0;
 	arg->width		= 0;
+	arg->has_precision = 0;
 	arg->precision	= 0;
 	arg->length		= '\0';
 	arg->specifier	= '\0';
@@ -13,19 +14,19 @@ t_arg	parse_arg(t_printf *pr)
 {
 	t_arg arg;
 
-	arg.flags		= parse_flags(pr);
-	arg.width		= parse_width(pr);
-	arg.precision	= parse_precision(pr);
-	arg.length		= parse_length(pr);
-	arg.specifier	= parse_specifier(pr);
-
-	return arg;
+	parse_flags(pr, &arg);
+	parse_width(pr, &arg);
+	parse_precision(pr, &arg);
+	parse_length(pr, &arg);
+	parse_specifier(pr, &arg);
+	return (arg);
 }
 
-long	parse_flags(t_printf *pr)
+void	parse_flags(t_printf *pr, t_arg *arg)
 {
-	long	flags = 0;
+	long int    flags;
 
+	flags = 0;
 	while (corresponding(pr->fmt[pr->i], "-+ #0"))
 	{
 		if (pr->fmt[pr->i] == '-')
@@ -38,33 +39,46 @@ long	parse_flags(t_printf *pr)
 			flags |= SHOW_PREFIX;
 		if (pr->fmt[pr->i] == '0')
 			flags |= PAD_ZERO;
-
 		pr->i += 1;
 	}
-
-	return flags;
+	arg->flags = flags;
 }
 
-int		parse_width(t_printf *pr)
+void	parse_width(t_printf *pr, t_arg *arg)
 {
-	int		width = 0;
+	int	width;
 
+	width = 0;
 	while (corresponding(pr->fmt[pr->i], "*0123456789"))
 	{
 		if (pr->fmt[pr->i] == '*')
 			width = va_arg(pr->args, int);
 		else
-		{
 			width = width * 10 + (pr->fmt[pr->i] - '0');
-		}
 		pr->i += 1;
 	}
-
-	return (width);
+	arg->width = width;
 }
 
-int		parse_precision(t_printf *pr)
+void	parse_precision(t_printf *pr, t_arg *arg)
 {
+	int precision;
+
+	if (pr->fmt[pr->i] == '.')
+	{
+		arg->has_precision = 1;
+		pr->i += 1;
+	}
+	precision = 0;
+	while (corresponding(pr->fmt[pr->i], "*0123456789"))
+	{
+		if (pr->fmt[pr->i] == '*')
+			precision = va_arg(pr->args, int);
+		else
+			precision = precision * 10 + (pr->fmt[pr->i] - '0');
+		pr->i += 1;
+	}
+	arg->precision = precision;
 }
 
 char	parse_length(t_printf *pr)
