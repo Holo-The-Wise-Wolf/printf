89b1cce27b43e22e5f9ef5bcffb49d5ec38a326f
lmoi <s4kaluu@gmail.com>
2020-10-19 20:21:09 +0200

    adds a few tests for parse_width

--- tests/parser.c
+++ tests/parser.c
@@ -3,11 +3,27 @@
 void	test_parse_flags(const char *fmt, long expected)
 {
 	t_printf	pr;
+	t_arg 		arg;
 	t_printf_init(&pr, fmt);
 
-	long result =  parse_flags(&pr);
-
-	printf("%d\t==\t%d\t", result, expected);
+	parse_flags(&pr, &arg);
+	long result =  arg.flags;
+	printf("%d\t==\t%d\n", result, expected);
 	assert(result == expected);
-	printf("\t=> OK!");
+	printf("\t=> OK!\n");
+}
+
+void	test_parse_width(const char *fmt, int expected, ...)
+{
+	t_printf	pr;
+	t_arg 		arg;
+	t_printf_init(&pr, fmt);
+	va_start(pr.args, fmt);
+
+	parse_width(&pr, &arg);
+	int result = arg.width;
+	printf("%d\t==\t%d\n", result, expected);
+	assert(result == expected);
+	printf("\t=> OK!\n");
+	va_end(pr.args);
 }
